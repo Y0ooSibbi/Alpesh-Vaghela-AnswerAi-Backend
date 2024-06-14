812c12245340562edfb2dde2d7abc51081e88319
ALPESH VAGHELA <alpesh57678@gmail.com>
2024-06-14 09:29:39 +0530

    Initial commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ email, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: 'User created' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.logout = (req, res) => {
+    res.json({ message: 'Logged out' });
+};
+
+exports.refresh = (req, res) => {
+    const { token } = req.body;
+    if (!token) return res.status(400).json({ message: 'No token provided' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+
+        const newToken = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token: newToken });
+    });
+};
